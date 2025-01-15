c1bedfb9c8efd56dfa01103615c8e6f93cbc041e
HobieV <145395563+HobieTheThird@users.noreply.github.com>
2025-01-15 21:06:21 +0000

    hw1

--- homework.cpp
+++ homework.cpp
@@ -3,6 +3,7 @@
 * contains the following procedures to work with lists:
 * display, append, insert, remove, length
 *
+*/
 #include "listhelpers.hpp"
 
 #include <cstdlib>   // srand, rand
@@ -11,10 +12,31 @@
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
 
+
+//Compare 
+void Compare(int x, int y)
+{
+    if (x < y)
+    {
+        std::cout << x << " is less than " << y << std::endl;
+    }
+    else
+    {
+        std::cout << x << " is greater than " << y << std::endl;
+    }
+}
+
+//Polynomial
+void quadForm(int a, int b, int c)
+{
+
+}
+
+
 int main()
 {
     // Complete your homework here
-
+    Compare(3,4);
     
     return 0;
 }
