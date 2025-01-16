99d862ac89586469b67ae4cccf5b851a0b1df4a3
HobieV <145395563+HobieTheThird@users.noreply.github.com>
2025-01-16 21:50:13 +0000

    quadform1 error

--- homework.cpp
+++ homework.cpp
@@ -6,6 +6,7 @@
 */
 #include "listhelpers.hpp"
 
+#include <cmath>
 #include <cstdlib>   // srand, rand
 #include <ctime>     // time
 #include <iostream>  // std::cin, std::cout
@@ -26,17 +27,38 @@ void Compare(int x, int y)
     }
 }
 
-//Polynomial
+//Quadratic
 void quadForm(int a, int b, int c)
 {
-
+    std::cout << "Pls enter 3 real numbers ";
+    std::cin >> a >> b >> c >> std::endl;
+    double sqrt(double number) 
+    {
+    // Checking for negative number input
+    if (number < 0) 
+    {
+        std::cout << "Error: Negative input is not allowed." << std::endl;
+        return -1; // Indicating error
+    }
+    
+    double sqrted = sqrt(b*b-4*a*c);
+    int num1 = (-b+sqrted)/(2*a);
+    int num2 = (-b - sqrted)/(a*2);
+    std::cout << "The answers are " << num1 << " and " << num2 << std::endl;
+     //return sqrt(number); // Using the sqrt function
+}
 }
 
 
 int main()
 {
     // Complete your homework here
-    Compare(3,4);
+
+    //Compare:
+    //Compare(3,4);
+
+    //Quadratic
+    quadForm(3,4,5);
     
     return 0;
 }
